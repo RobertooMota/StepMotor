b2bd6622250a57ac4fc92b968dc17a8f624515e3
RobertooMota <robertomota.contato@gmail.com>
2021-05-18 23:19:14 -0300

    exemplo atualizado

--- examples/moveTo/moveTo.ino
+++ examples/moveTo/moveTo.ino
@@ -6,10 +6,13 @@ StepMotor mainMotor(2, 3, 4, 12, "mm"); //motorName (Step Pin, Dir Pin, Enb Pin,
 
 void setup()
 {
-    //nothing declare
+    mainMotor.SetSensorHome(5); //Sensor pin for home
 }
 
 void loop()
 {
     mainMotor.moveTo(1000, 1);  //motorName.function(distance, direction);
+    mainMotor.moveToHome();     //Machine go to sensor home
+    mainMotor.moveToZero();     //After homing, the "moveToZero", start fast speed and reduces above 1 metter of the distance
+    mainMotor.onlyMove(1);      //Only move. Parameter: 1 - one side    2 - other side.
 }
